4e804d211e7e6940dd89347b46a3256bb3852a49
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-10-12 01:19:48 +0600

    create first vue project

---
+++ first-vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <ashrafUzzaman msg="Welcome to Ashraf's World"></ashrafUzzaman>
+  </div>
+</template>
+
+<script>
+import ashrafUzzaman from "./components/hi.vue";
+
+export default {
+  name: "App",
+  components: {
+    ashrafUzzaman,
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
